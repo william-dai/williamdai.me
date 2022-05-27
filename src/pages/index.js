e12e55e044d8192944993f2779a9d6cfd86bfa00
William <45790024+william-dai@users.noreply.github.com>
2022-05-26 22:46:14 -0700

    Ignoring module for Webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import Navibar from '../components/navibar.js';
 import IntroCard from '../components/introcard.js';
 import Title from '../components/title.js';
-// import ScrollReveal from 'scrollreveal';
 
 import './style.css';
-// import { createGlobalStyle } from 'styled-components';
 
 const Home = () => {
+
     return (
         <div>
             <Navibar/>
