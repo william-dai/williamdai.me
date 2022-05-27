8fa2b58d13b88cbebe1a62d37adb1f6347740b7e
William <45790024+william-dai@users.noreply.github.com>
2022-05-26 23:37:47 -0700

    Converting style.css into globalStyle.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Navibar from '../components/navibar.js';
 import IntroCard from '../components/introcard.js';
 import Title from '../components/title.js';
+import GlobalStyle from '../styles/globalStyle.js';
 
-import './style.css';
 
 const Home = () => {
 
     return (
         <div>
+            <GlobalStyle/>
             <Navibar/>
             <Title/>
             <IntroCard/>
---
+++ src/styles/globalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        font-family: sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+
+    html, body {
+        background-color: rgba(250, 250, 250);
+    }
+
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+`;
+
+export default GlobalStyle;
